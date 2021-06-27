a093e86d5fd1cd9ecfb57923ff0fa91a0aa92b5d
Eduardo Anami <duhsegundo@hotmail.com>
2021-06-27 20:40:20 -0300

    chore: Aula 02.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <h1>Hello World</h1>
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { AuthContextProvider } from './contexts/AuthContext';
+
+function App() {  
+  return (    
+    <BrowserRouter>
+      <AuthContextProvider>
+        <Route path="/" exact component={ Home }></Route>
+        <Route path="/rooms/new" component={ NewRoom }></Route>      
+      </AuthContextProvider>
+    </BrowserRouter>
   );
 }
 
